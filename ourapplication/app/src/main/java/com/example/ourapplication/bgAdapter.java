a894dd849f40950e9b39b02502a54d441628af85
my l <1006564282@qq.com>
2018-11-27 19:17:10 +0800

    加入界面

---
+++ ourapplication/app/src/main/java/com/example/ourapplication/bgAdapter.java
@@ -0,0 +1,79 @@
+package com.example.ourapplication;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+/**
+ * Created by 10065 on 2018/11/1.
+ */
+
+public class bgAdapter extends RecyclerView.Adapter<bgAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<bgSet> mbgList;
+    private OnItemClickListener mItemClickListener;
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        ImageView bgImage;
+
+        public ViewHolder(View view){
+            super(view);
+            cardView = (CardView)view;
+            bgImage = (ImageView)view.findViewById(R.id.bgImage);
+        }
+    }
+
+    public bgAdapter(List<bgSet> bgList){
+        mbgList = bgList;
+    }
+
+    @Override
+    public bgAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if(mContext == null){
+            mContext = parent.getContext();
+        }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.background_layout, parent, false);
+        bgAdapter.ViewHolder holder = new bgAdapter.ViewHolder(view);
+        return holder;
+    }
+
+
+    public interface OnItemClickListener {
+        void onClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.mItemClickListener = listener;
+    }
+
+
+    @Override
+    public void onBindViewHolder(bgAdapter.ViewHolder holder, final int position) {
+        bgSet mybg = mbgList.get(position);
+        holder.bgImage.setImageResource(mybg.getId());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener(){   //item点击事件
+            @Override
+            public void onClick(View v) {
+                if(mItemClickListener!=null){
+                    mItemClickListener.onClick(position);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() { return mbgList.size(); }
+
+}
+
+
